205e1a2cf0361f3d48397a1fba885cb28b1fc33c
Marta Łoskot <loskotmarta@gmail.com>
2024-03-16 14:37:45 +0100

    Dropping task

--- js/app.js
+++ js/app.js
@@ -8,13 +8,13 @@ tasks.forEach( task => {
 })
 
 taskContainers.forEach( taskContainer => {
-    taskContainer.addEventListener('dragdrop', dragDrop)
+    taskContainer.addEventListener('drop', dragDrop)
     taskContainer.addEventListener('dragover', dragOver)
 })
 
 function dragStart(e) {
+    console.log(e.target)
     taskDragged = e.target
-    console.log(taskDragged)
 }
 
 function dragOver(e) {
@@ -22,41 +22,8 @@ function dragOver(e) {
 }
 
 function dragDrop(e) {
-  console.log(e.target)
+    console.log(e.target)
+    e.target.append(taskDragged) 
 }
 
-
-
-
-
-
-
-
-
-
-
-
-// const tasks = document.querySelectorAll('.task')
-// const taskContainers = document.querySelectorAll('.task-container')
-
-// let taskDragged
-
-// tasks.forEach( task => task.addEventListener('dragstart', dragStart))
-// function dragStart(e) {
-//     taskDragged = e.target
-   
-// }
-
-// taskContainers.forEach(taskContainer => {
-//     taskContainer.addEventListener('drop', dropTask)
-//     taskContainer.addEventListener('dragover', dragTask)
-// })
-
-// function dropTask(e) {
-//     e.target.append(taskDragged)
-// }
-
-// function dragTask(e) {
-//     e.preventDefault()
-// }
 
