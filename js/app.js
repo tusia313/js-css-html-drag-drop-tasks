52676cbe5192a4ebb4b15d8753cc012d004d749d
Marta Łoskot <loskotmarta@gmail.com>
2024-03-16 14:10:17 +0100

    Adding dragging functionality

--- js/app.js
+++ js/app.js
@@ -1,24 +1,62 @@
-const tasks = document.querySelectorAll('.task')
 const taskContainers = document.querySelectorAll('.task-container')
+const tasks = document.querySelectorAll('.task')
 
 let taskDragged
 
-tasks.forEach( task => task.addEventListener('dragstart', dragStart))
-function dragStart(e) {
-    taskDragged = e.target
-   
-}
-
-taskContainers.forEach(taskContainer => {
-    taskContainer.addEventListener('drop', dropTask)
-    taskContainer.addEventListener('dragover', dragTask)
+tasks.forEach( task => {
+    task.addEventListener('dragstart', dragStart)
 })
 
-function dropTask(e) {
-    e.target.append(taskDragged)
+taskContainers.forEach( taskContainer => {
+    taskContainer.addEventListener('dragdrop', dragDrop)
+    taskContainer.addEventListener('dragover', dragOver)
+})
+
+function dragStart(e) {
+    taskDragged = e.target
+    console.log(taskDragged)
 }
 
-function dragTask(e) {
+function dragOver(e) {
     e.preventDefault()
 }
+
+function dragDrop(e) {
+  console.log(e.target)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// const tasks = document.querySelectorAll('.task')
+// const taskContainers = document.querySelectorAll('.task-container')
+
+// let taskDragged
+
+// tasks.forEach( task => task.addEventListener('dragstart', dragStart))
+// function dragStart(e) {
+//     taskDragged = e.target
+   
+// }
+
+// taskContainers.forEach(taskContainer => {
+//     taskContainer.addEventListener('drop', dropTask)
+//     taskContainer.addEventListener('dragover', dragTask)
+// })
+
+// function dropTask(e) {
+//     e.target.append(taskDragged)
+// }
+
+// function dragTask(e) {
+//     e.preventDefault()
+// }
 
